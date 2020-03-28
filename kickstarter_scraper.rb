a8cc6af7bc43b80e7b53960cf51351649bf76f10
Christopher Osborn <z68ozz@gmail.com>
2020-03-28 03:26:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,10 +3,10 @@ require "nokogiri"
 require "pry"
 
 
-html = File.read('fixtures/kickstarter.html')
+
 
 kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
-  
+  html = File.read('fixtures/kickstarter.html')
 end
